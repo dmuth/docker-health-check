ce8b83551d80f9ec347cb41593e9a9df1ac5739f
Douglas Muth <dmuth@dmuth.org>
2019-05-05 13:54:13 -0400

    Added ability to chose a port.

--- start-nginx.sh
+++ start-nginx.sh
@@ -6,17 +6,36 @@
 # Errors are fatal
 set -e
 
-NAME="test_nginx"
+NAME="docker_test_nginx"
 PORT=8123
 
+if test "$1" == "-h" -o "$1" == "--help"
+then
+	echo "! "
+	echo "! Syntax: $0 [ port ]"
+	echo "! "
+	exit 1
+fi
+
+if test "$1"
+then
+	PORT=$1
+fi
+
+echo "# "
+echo "# If Nginx container was previously running, I will now kill it..."
+echo "# "
 ID=$(docker kill $NAME || true)
 
 echo "# "
-echo "# Starting nginx..."
+echo "# Starting Nginx conatiner, listening on port ${PORT}..."
 echo "# "
 
 ID=""
 
+#
+# Try once every second to start Nginx, until we succeed.
+#
 while test ! "$ID"
 do
 	ID=$(docker run -d --name $NAME -p $PORT:80 --rm nginx || true)
@@ -24,7 +43,7 @@ do
 done
 
 echo "# "
-echo "# Nginx started!"
+echo "# Nginx started, and listenong on port ${PORT}!"
 echo "# "
 
 
